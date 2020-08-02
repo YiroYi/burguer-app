612b62a1e10ccc97ca6314c5c5d89d27b240f4f4
YiroYi <yiroyi@live.com>
2020-08-01 21:01:26 -0500

    Frontend complete

--- src/components/Burguer/OrderSummary/OrderSummary.js
+++ src/components/Burguer/OrderSummary/OrderSummary.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients)
-  .map((igKey) => {
-    return (<li key={igKey}>
-              <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li>)
-  });
-  return(
-    <Aux>
-      <h3>Your order</h3>
-      <p>A delicious burguer with</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p>Total Price: {props.price.toFixed(2)}</p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>Continue</Button>
-    </Aux>
-    );
-};
+class OrderSummary extends Component {
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients)
+    .map((igKey) => {
+      return (<li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+              </li>)
+    });
+    return(
+      <Aux>
+        <h3>Your order</h3>
+        <p>A delicious burguer with</p>
+        <ul>
+          {ingredientsSummary}
+        </ul>
+        <p>Total Price: {this.props.price.toFixed(2)}</p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>Continue</Button>
+      </Aux>
+      );
+  }
+}
+
 
 export default OrderSummary;
