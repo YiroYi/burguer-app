50b42783d278bd389bb714048150328ca30e6d84
YiroYi <yiroyi@live.com>
2020-08-03 09:30:58 -0500

    Implementing routing to the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
+import Checkout from './containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurguerBuilder />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurguerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
