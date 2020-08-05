d3527b35bcb86df8de8519a6dfbb7ea8320804fc
YiroYi <yiroyi@live.com>
2020-08-05 16:47:42 -0500

    Validation added to form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -14,7 +14,12 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Your name'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false,
+          touched: false
         },
         street: {
           elementType: 'input',
@@ -22,15 +27,28 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Your street'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false,
+          touched: false
+
         },
         zipCode: {
           elementType: 'input',
           elementConfig: {
             type: 'text',
-            placeholder: 'Your zipCode'
+            placeholder: 'Your zipCode',
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true,
+            minLength: 5,
+            maxLength: 5
+          },
+          valid: false,
+          touched: false
         },
         country: {
           elementType: 'input',
@@ -38,7 +56,12 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Your country'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false,
+          touched: false
         },
         email: {
           elementType: 'input',
@@ -46,7 +69,12 @@ class ContactData extends Component {
             type: 'text',
             placeholder: 'Your email'
           },
-          value: ''
+          value: '',
+          validation: {
+            required: true
+          },
+          valid: false,
+          touched: false
         },
         deliveryMethod: {
           elementType: 'select',
@@ -54,9 +82,14 @@ class ContactData extends Component {
             options: [{value: 'fastest', displayValue: 'Fastest'},
                       {value: 'cheapest', displayValue: 'Cheapest'}]
           },
-          value: ''
+          validation: {
+            required: false
+          },
+          value: '',
+          valid: true
         }
     },
+    formIsValid: false,
     loading: false
   }
 
@@ -64,9 +97,17 @@ class ContactData extends Component {
     event.preventDefault();
 
     this.setState({loading: true});
+
+    const formData = {};
+
+    for(let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
+
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      orderData: formData
     }
 
     axios.post('/orders.json', order)
@@ -77,6 +118,48 @@ class ContactData extends Component {
     .catch(error => {
       this.setState({ loading: false });
     });
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if(rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if(rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if(rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }; // clonando el objecto completo
+
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    }; // clonando un elemento del objecto esto lo debemos hacer
+    //porque hay elementos que estan nested.
+
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+    let formIsValid = true;
+    for(let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+    }
+
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
+
   }
 
   render() {
@@ -91,15 +174,21 @@ class ContactData extends Component {
     }
 
     let form = (
-        <form>
+        <form onSubmit={this.orderHandler}>
           {formElementsArray.map(formElement => (
             <Input
               key={formElement.id}
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
-              value={formElement.config.value}/>
+              value={formElement.config.value}
+              invalid={!formElement.config.valid}
+              touched = {formElement.config.touched}
+              shouldValidate={formElement.config.validation}
+              changed={ (event) => this.inputChangedHandler(event, formElement.id)}/>
           ))}
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+          <Button btnType="Success"
+                  disabled={!this.state.formIsValid}
+          >ORDER</Button>
         </form>);
 
     if (this.state.loading) {
