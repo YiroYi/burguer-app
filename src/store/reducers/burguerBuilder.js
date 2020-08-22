9562a3fc14589cc2f6aaaf5771bce4366eaca396
YiroYi <yiroyi@live.com>
2020-08-21 22:39:07 -0500

    Burger redirect

--- src/store/reducers/burguerBuilder.js
+++ src/store/reducers/burguerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from './utility';
 const initialState = {
   ingredients:  null,
   totalPrice: 4,
-  error: false
+  error: false,
+  building: false
 };
 
 const INGREDIENT_PRICES = {
@@ -21,7 +22,8 @@ const reducer = (state = initialState, action) => {
       const updatedIngredients = updateObject(state.ingredients, updatedIngredient)
       const updatedState = {
         ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+        building: true
       }
       return updateObject(state, updatedState);
 
@@ -30,7 +32,8 @@ const reducer = (state = initialState, action) => {
         ...state,
         ingredients: action.ingredients,
         totalPrice: 4,
-        error: false
+        error: false,
+        building: false
       };
 
     case actionTypes.FETCH_INGREDIENTS_FAILED:
@@ -46,7 +49,8 @@ const reducer = (state = initialState, action) => {
           ...state.ingredients, //Copy the properties of the old state
           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
         },
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+        building: true
       };
     default:
       return state;
